7183becf193b3c864446e30be561bd371a5f21dc
Victor <victoralexsander5200@gmail.com>
2022-11-16 13:09:21 -0300

    feat: att

--- src/app/cadastro/cadastro-routing.module.ts
+++ src/app/cadastro/cadastro-routing.module.ts
@@ -1,7 +1,10 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {UsuarioComponent} from "./usuario/usuario.component";
-<<<<<<< HEAD
+import {VeiculoComponent} from "./veiculo/veiculo.component";
+import {MidiaComponent} from "./midia/midia.component";
+import {TierComponent} from "./tier/tier.component";
+import {FatorAlcanceComponent} from "./fator-alcance/fator-alcance.component";
 import {SentimentoComponent} from "./sentimento/sentimento.component";
 import {ProatividadeComponent} from "./proatividade/proatividade.component";
 import {ExposicaoComponent} from "./exposicao/exposicao.component";
@@ -12,21 +15,12 @@ const routes: Routes = [
   { path: 'sentimento', component: SentimentoComponent},
   { path: 'proatividade', component: ProatividadeComponent},
   { path: 'exposicao', component: ExposicaoComponent},
-  { path: 'cliente', component: ClienteComponent}
-=======
-import {VeiculoComponent} from "./veiculo/veiculo.component";
-import {MidiaComponent} from "./midia/midia.component";
-import {TierComponent} from "./tier/tier.component";
-import {FatorAlcanceComponent} from "./fator-alcance/fator-alcance.component";
-
-const routes: Routes = [
-  { path: 'usuario', component: UsuarioComponent},
+  { path: 'cliente', component: ClienteComponent},
   { path: 'veiculo', component: VeiculoComponent},
   { path: 'midia', component: MidiaComponent},
   { path: 'tier', component: TierComponent},
-  { path: 'fda', component: FatorAlcanceComponent}
->>>>>>> main
-];
+  { path: 'fda', component: FatorAlcanceComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
