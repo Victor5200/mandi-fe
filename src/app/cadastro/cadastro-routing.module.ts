4103cfb2dd426ca1af668d99a86a85513eaddbc9
Victor <victoralexsander5200@gmail.com>
2022-11-16 12:43:07 -0300

    feat-iago: att

--- src/app/cadastro/cadastro-routing.module.ts
+++ src/app/cadastro/cadastro-routing.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 import {UsuarioComponent} from "./usuario/usuario.component";
-import {ProatividadeComponent} from "./proatividade/proatividade.component";
 import {SentimentoComponent} from "./sentimento/sentimento.component";
+import {ProatividadeComponent} from "./proatividade/proatividade.component";
 import {ExposicaoComponent} from "./exposicao/exposicao.component";
 import {ClienteComponent} from "./cliente/cliente.component";
 
 const routes: Routes = [
   { path: 'usuario', component: UsuarioComponent},
-  { path: 'proatividade', component: ProatividadeComponent},
   { path: 'sentimento', component: SentimentoComponent},
+  { path: 'proatividade', component: ProatividadeComponent},
   { path: 'exposicao', component: ExposicaoComponent},
   { path: 'cliente', component: ClienteComponent}
 ];
