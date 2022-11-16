d4eb5a3dae8f30759c7f738ccb905844825a057a
Victor <victoralexsander5200@gmail.com>
2022-11-16 12:58:19 -0300

    Feat-Eduardo: telas-fda,tier e midia

--- src/app/cadastro/cadastro-routing.module.ts
+++ src/app/cadastro/cadastro-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UsuarioComponent} from "./usuario/usuario.component";
 import {VeiculoComponent} from "./veiculo/veiculo.component";
+import {MidiaComponent} from "./midia/midia.component";
+import {TierComponent} from "./tier/tier.component";
+import {FatorAlcanceComponent} from "./fator-alcance/fator-alcance.component";
 
 const routes: Routes = [
-  { path: '', component: UsuarioComponent},
-  { path: 'veiculo', component: VeiculoComponent}
+  { path: 'usuario', component: UsuarioComponent},
+  { path: 'veiculo', component: VeiculoComponent},
+  { path: 'midia', component: MidiaComponent},
+  { path: 'tier', component: TierComponent},
+  { path: 'fda', component: FatorAlcanceComponent}
 ];
 
 @NgModule({
